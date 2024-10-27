aba572c837e7e0b6af37398a6db1b8352452fc73
全金属雪糕 <1637096329@qq.com>
2024-10-27 16:29:08 +0000

    update alipay-server/src/test/java/com/satan/pay/CheckPickTest.java.
    Signed-off-by: 全金属雪糕 <1637096329@qq.com>

--- alipay-server/src/test/java/com/satan/pay/CheckPickTest.java
+++ alipay-server/src/test/java/com/satan/pay/CheckPickTest.java
@@ -9,8 +9,7 @@ public class CheckPickTest {
     public void role() {
         System.out.println("创建了role服务");
     }
-
-    public void image() {
-        System.out.println("创建了image服务");
+    public void icon() {
+        System.out.println("创建了icon服务");
     }
 }
