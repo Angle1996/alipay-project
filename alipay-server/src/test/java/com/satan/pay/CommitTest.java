dbfcb01ff0dcf3c1060fd357318a3d70c5ff305f
全金属雪糕 <1637096329@qq.com>
2024-10-28 00:29:08 +0800

    添加功能1

---
+++ alipay-server/src/test/java/com/satan/pay/CommitTest.java
@@ -0,0 +1,12 @@
+package com.satan.pay;
+
+/**
+ * @Author: Demon
+ * @Date: 2024/10/28 23:24
+ * @Description:
+ **/
+public class CommitTest {
+    public void test1() {
+        System.out.println("功能1");
+    }
+}
