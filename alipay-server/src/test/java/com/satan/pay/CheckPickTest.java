ec9d214b91094af06d6d4e0b802de2cdb4416fca
全金属雪糕 <1637096329@qq.com>
2024-10-27 16:07:17 +0000

    增加check功能
    Signed-off-by: 全金属雪糕 <1637096329@qq.com>

--- alipay-server/src/test/java/com/satan/pay/CheckPickTest.java
+++ alipay-server/src/test/java/com/satan/pay/CheckPickTest.java
@@ -9,6 +9,9 @@ public class CheckPickTest {
     public void role() {
         System.out.println("role功能");
     }
+    public void check() {
+        System.out.println("check功能");
+    }
 
     public void images() {
         System.out.println("images功能");
