c790830782e4658abe2d5b0771c2f6d50ad53583
Demon <1637096329@qq.com>
2024-10-28 00:27:42 +0800

    创建了role服务

--- alipay-server/src/test/java/com/satan/pay/CheckPickTest.java
+++ alipay-server/src/test/java/com/satan/pay/CheckPickTest.java
@@ -2,24 +2,11 @@ package com.satan.pay;
 
 /**
  * @Author: Demon
- * @Date: 2024/10/28 00:03
+ * @Date: 2024/10/28 00:26
  * @Description:
  **/
 public class CheckPickTest {
     public void role() {
-        System.out.println("role功能");
+        System.out.println("创建了role服务");
     }
-
-    public void images() {
-        System.out.println("images功能");
-    }
-
-    public void video() {
-        System.out.println("video功能");
-    }
-
-    public void music() {
-        System.out.println("music功能");
-    }
-
 }
