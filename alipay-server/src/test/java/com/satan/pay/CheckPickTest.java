9d224602b6b9ff675a868087e0971309eb461cfc
Demon <1637096329@qq.com>
2024-10-28 00:05:30 +0800

    增加role功能

---
+++ alipay-server/src/test/java/com/satan/pay/CheckPickTest.java
@@ -0,0 +1,14 @@
+package com.satan.pay;
+
+/**
+ * @Author: Demon
+ * @Date: 2024/10/28 00:03
+ * @Description:
+ **/
+public class CheckPickTest {
+    public void role() {
+        System.out.println("role功能");
+    }
+
+
+}
